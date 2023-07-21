911dac761ed23ed2c209b0c14590e60c5c36d3f0
Alireza khadembashi <ali.khadembashi@protonmail.com>
2023-07-21 21:45:36 +0330

    hlfway p2l11 creating server

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const fs  = require('fs');
+const http  = require('http')
+
+
+//SERVER
+const server = http.createServer((req,res)=>{
+    res.end('welcome to your first node server')
+})
+
+server.listen('8080','127.0.0.1')
+
+
+
+// FILE SYSTEM
 //Blocking (synchronous)
-const myText  = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(myText)
-fs.writeFileSync("./txt/output.txt", 'this is my text n then some is it gonna be a new file?')
+// const myText  = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(myText)
+// fs.writeFileSync("./txt/output.txt", 'this is my text n then some is it gonna be a new file?')
 
 //non-blocking(asynchronous)
 // fs.readFile('./txt/starter.txt','utf-8', (err,data)=>{
@@ -16,12 +29,12 @@ fs.writeFileSync("./txt/output.txt", 'this is my text n then some is it gonna be
 // })
 
 // async promise-based
-const readFile = async()=>{
-    try {
-        const data  = await fs.promises.readFile('./txt/start.txt','utf-8')
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
-readFile()
+// const readFile = async()=>{
+//     try {
+//         const data  = await fs.promises.readFile('./txt/start.txt','utf-8')
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// readFile()
