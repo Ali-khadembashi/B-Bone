175b4ba139fa588e52fba3282a11c8d2c301ac64
Alireza khadembashi <ali.khadembashi@protonmail.com>
2023-07-21 17:11:43 +0330

    till p2l10

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const fs  = require('fs');
-fs.copyFileSync()
+//Blocking
+const myText  = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(myText)
+fs.writeFileSync("./txt/output.txt", 'this is my text n then some is it gonna be a new file?')
+
+//non-blocking
