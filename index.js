03097d78605decd7d829c860e74adc9faceedb73
Alireza khadembashi <ali.khadembashi@protonmail.com>
2023-07-21 18:02:48 +0330

    async fs.promises.readFile p2l10

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const fs  = require('fs');
-//Blocking
+//Blocking (synchronous)
 const myText  = fs.readFileSync('./txt/input.txt', 'utf-8')
 console.log(myText)
 fs.writeFileSync("./txt/output.txt", 'this is my text n then some is it gonna be a new file?')
 
-//non-blocking
+//non-blocking(asynchronous)
+// fs.readFile('./txt/starter.txt','utf-8', (err,data)=>{
+//     if(err){
+//         console.log(err);
+
+//     }else if(!err){
+//         console.log(data);
+//     }
+    
+// })
+
+// async promise-based
+const readFile = async()=>{
+    try {
+        const data  = await fs.promises.readFile('./txt/start.txt','utf-8')
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+readFile()
