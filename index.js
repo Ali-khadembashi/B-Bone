619e7c97f8f709785da1097d3cafa0e0983aa24d
a.khadembashi <a.khadembashi@DATX81_SOFT>
2023-08-08 11:01:18 +0330

    parsing query object l2P16

--- index.js
+++ index.js
@@ -1,84 +1,98 @@
-const fs  = require('fs');
-const http  = require('http');
-const { stringify } = require('querystring');
-const url  = require('url')
-const readFile = async (path, format)=>{
-try {
-  const data  =  await fs.promises.readFile(`${__dirname}/dev-data/data.json`,'utf-8')
-  return JSON.parse(data)
-} catch (error) {
-  
-}
-}
+const fs = require("fs");
+const http = require("http");
+const { stringify } = require("querystring");
+const url = require("url");
+const readFile = async (path, format) => {
+  try {
+    const data = await fs.promises.readFile(
+      `${__dirname}/dev-data/data.json`,
+      "utf-8"
+    );
+    return JSON.parse(data);
+  } catch (error) {}
+};
 //SERVER
-const tempOverview  = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard  = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct  = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-let data = ''
-readFile().then(data=>{
-  console.log(data);
-})
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    const pathname = req.url
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+let data = "";
+// readFile().then(data=>{
+//   console.log(data);
+// })
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  // const pathname = req.url
+  // console.log(url.parse(req.url));
+  // console.log("-------");
+  // console.log(url.parse(req.url, true));
+  const { query, pathname } = url.parse(req.url, true);
 
-    
-    
-    if(pathname==='/home'){
-      
-      res.end('welcome home!')
-      // overview page
-    }else if (pathname==='/'){
-      readFile().then(data=>{
-        console.log(data);
-       const cardsHtml =  data.map(item=>{
-        return(
-          tempCard.replaceAll('{%PRODUCTNAME%}',item.productName).replaceAll('{%ID%}',item.id).replaceAll('{%QUANTITY%}',item.quantity).replaceAll('{%IMAGE%}',item.image)
-        )
-       })
-       res.writeHead(200,{'Content-type':'text/html'})
-       res.end(tempOverview.replaceAll('{%PRODUCT_CARDS%}',cardsHtml))
-      })
-     
-      
-        //product page
-    }else if (pathname==='/product'){
+  if (pathname === "/home") {
+    res.end("welcome home!");
+    // overview page
+  } else if (pathname === "/") {
+    readFile().then((data) => {
+      // console.log(data);
+      const cardsHtml = data.map((item) => {
+        return tempCard
+          .replaceAll("{%PRODUCTNAME%}", item.productName)
+          .replaceAll("{%ID%}", item.id)
+          .replaceAll("{%QUANTITY%}", item.quantity)
+          .replaceAll("{%IMAGE%}", item.image);
+      });
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.end(tempOverview.replaceAll("{%PRODUCT_CARDS%}", cardsHtml));
+    });
 
-        // api
-    }else if (pathname==='/api'){
-      res.writeHead(200,{
-        'Content-type':'application/json'
-      })
-      readFile().then(data=> res.end(JSON.stringify(data)))
-     
-      console.log(readFile());
-    
-    }else{
-        res.writeHead(404,{
-            'Content-type':'text/html',
-            'My-custom-header':'new type of header'
-        })
-        res.end('<h1>404 page not foud</h1>')
-    }
-    
+    //product page
+  } else if (pathname === "/product") {
+    readFile().then((data) => {
+      const [pd] = data.filter((item) => item.id === JSON.parse(query.id));
+      console.log(pd);
+      console.log(query.id);
+      res.writeHead(200, { "Content-type": "text/html" });
+      // res.end('<div>asd</div>')
+      res.end(
+        tempProduct
+          .replaceAll("{%PRODUCTNAME%}", pd?.productName)
+          .replaceAll("{%PRICE%}",pd?.price)
+          .replaceAll("{%QUANTITY%}",pd?.quantity)
+          .replaceAll("{%FROM%}",pd?.from)
+          .replaceAll("{%DESCRIPTION%}",pd?.description)
+          .replaceAll("{%IMAGE%}",pd?.image)
+          .replaceAll("{%ORGANIC%}",pd?.organic)
+      );
+    });
 
-})
+    // api
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    readFile().then((data) => res.end(JSON.stringify(data)));
 
-server.listen('8080','127.0.0.1', ()=>{
-    console.log('server is running');
-})
+    console.log(readFile());
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "My-custom-header": "new type of header",
+    });
+    res.end("<h1>404 page not foud</h1>");
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
+server.listen("8080", "127.0.0.1", () => {
+  console.log("server is running");
+});
 
 // FILE SYSTEM
 //Blocking (synchronous)
@@ -94,7 +108,7 @@ server.listen('8080','127.0.0.1', ()=>{
 //     }else if(!err){
 //         console.log(data);
 //     }
-    
+
 // })
 
 // async promise-based
@@ -108,78 +122,45 @@ server.listen('8080','127.0.0.1', ()=>{
 // }
 // readFile()
 
-
-
-
-
-
-
-
 class CustomType {
-    constructor(name, code) {
-      this.name = name;
-      this.code = code;
-    }
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
   }
-  
-  function processInput(input) {
-    if (Array.isArray(input)) {
-      // Handle array input
-      input.forEach(item => {
-        if (item.name && item.code) {
-          // Process each item in the array
-          console.log(`Name: ${item.name}, Code: ${item.code}`);
-        }
-      });
-    } else if (typeof input === 'object' && input.type) {
-      // Handle object input
-      const { name, number } = input.type;
-      console.log(`Name: ${name}, Number: ${number}`);
-    } else {
-      // Invalid input
-      console.log('Invalid input');
-    }
+}
+
+function processInput(input) {
+  if (Array.isArray(input)) {
+    // Handle array input
+    input.forEach((item) => {
+      if (item.name && item.code) {
+        // Process each item in the array
+        console.log(`Name: ${item.name}, Code: ${item.code}`);
+      }
+    });
+  } else if (typeof input === "object" && input.type) {
+    // Handle object input
+    const { name, number } = input.type;
+    console.log(`Name: ${name}, Number: ${number}`);
+  } else {
+    // Invalid input
+    console.log("Invalid input");
   }
-  
-  // Example usage
-  const arrayInput = [
-    { name: 'String 1', code: 123 },
-    { name: 'String 2', code: 456 },
-    { name: 'String 3', code: 789 }
-  ];
-  
-  const objectInput = {
-    type: {
-      name: 'Some String',
-      number: 123
-    }
-  };
-  
-  processInput(arrayInput);
-  processInput(objectInput);
+}
 
+// Example usage
+const arrayInput = [
+  { name: "String 1", code: 123 },
+  { name: "String 2", code: 456 },
+  { name: "String 3", code: 789 },
+];
 
+const objectInput = {
+  type: {
+    name: "Some String",
+    number: 123,
+  },
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+processInput(arrayInput);
+processInput(objectInput);
